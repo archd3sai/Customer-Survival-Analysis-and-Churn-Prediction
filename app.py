49865a71b000ce454146b2b0817d6aa9ee6e6331
Arch Desai <archdesai.ad@gmail.com>
2020-02-27 09:06:50 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle, Wedge, Rectangle
+import time
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
@@ -26,7 +29,22 @@ def predict():
         PaperlessBilling = 1
 
     MonthlyCharges = int(request.form["MonthlyCharges"])
-    tenure_group = int(request.form["Tenure"])
+
+    def tenure(t):
+        if t<=12:
+            return 1
+        elif t>12 and t<=24:
+            return 2
+        elif t>24 and t<=36:
+            return 3
+        elif t>36 and t<=48:
+            return 4
+        elif t>48 and t<=60:
+            return 5
+        else:
+            return 6
+
+    tenure_group = tenure(int(request.form["Tenure"]))
 
     InternetService_Fiberoptic = 0
     InternetService_No = 0
@@ -102,16 +120,92 @@ def predict():
 
     output = prediction[0,1]
 
-    return render_template('index.html', prediction_text='The Probability of customer churn: {}'.format(round(output, 2)))
+    def degree_range(n):
+        start = np.linspace(0,180,n+1, endpoint=True)[0:-1]
+        end = np.linspace(0,180,n+1, endpoint=True)[1::]
+        mid_points = start + ((end-start)/2.)
+        return np.c_[start, end], mid_points
 
-@app.route('/results',methods=['POST'])
-def results():
+    def rot_text(ang):
+        rotation = np.degrees(np.radians(ang) * np.pi / np.pi - np.radians(90))
+        return rotation
 
-    data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
+    def gauge(labels=['LOW','MEDIUM','HIGH','EXTREME'], \
+              colors=['#007A00','#0063BF','#FFCC00','#ED1C24'], Probability=1, fname=False):
 
-    output = prediction[0]
-    return jsonify(output)
+        N = len(labels)
+        colors = colors[::-1]
+
+
+        """
+        begins the plotting
+        """
+
+        fig, ax = plt.subplots()
+
+        ang_range, mid_points = degree_range(4)
+
+        labels = labels[::-1]
+
+        """
+        plots the sectors and the arcs
+        """
+        patches = []
+        for ang, c in zip(ang_range, colors):
+            # sectors
+            patches.append(Wedge((0.,0.), .4, *ang, facecolor='w', lw=2))
+            # arcs
+            patches.append(Wedge((0.,0.), .4, *ang, width=0.10, facecolor=c, lw=2, alpha=0.5))
+
+        [ax.add_patch(p) for p in patches]
+
+
+        """
+        set the labels (e.g. 'LOW','MEDIUM',...)
+        """
+
+        for mid, lab in zip(mid_points, labels):
+
+            ax.text(0.35 * np.cos(np.radians(mid)), 0.35 * np.sin(np.radians(mid)), lab, \
+                horizontalalignment='center', verticalalignment='center', fontsize=14, \
+                fontweight='bold', rotation = rot_text(mid))
+
+        """
+        set the bottom banner and the title
+        """
+        r = Rectangle((-0.4,-0.1),0.8,0.1, facecolor='w', lw=2)
+        ax.add_patch(r)
+
+        ax.text(0, -0.05, 'Churn Probability ' + np.round(Probability,2).astype(str), horizontalalignment='center', \
+             verticalalignment='center', fontsize=22, fontweight='bold')
+
+        """
+        plots the arrow now
+        """
+
+        pos = (1-Probability)*180
+        ax.arrow(0, 0, 0.225 * np.cos(np.radians(pos)), 0.225 * np.sin(np.radians(pos)), \
+                     width=0.04, head_width=0.09, head_length=0.1, fc='k', ec='k')
+
+        ax.add_patch(Circle((0, 0), radius=0.02, facecolor='k'))
+        ax.add_patch(Circle((0, 0), radius=0.01, facecolor='w', zorder=11))
+
+        """
+        removes frame and ticks, and makes axis equal and tight
+        """
+
+        ax.set_frame_on(False)
+        ax.axes.set_xticks([])
+        ax.axes.set_yticks([])
+        ax.axis('equal')
+        plt.tight_layout()
+
+        plt.savefig('static/images/new_plot.png')
+
+    gauge(Probability = output)
+    t = time.time()
+    return render_template('index.html', prediction_text='The Probability of customer churn: {}'.format(round(output, 2)), url ='static/images/new_plot.png?{}'.format(t))
+
 
 if __name__ == "__main__":
     app.run(port = 8000, debug=True)
